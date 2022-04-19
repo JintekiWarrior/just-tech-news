63a881a93a9649cc6789ba5ead2d9a2cf4908d31
JintekiWarrior <jintekiwarrior@gmail.com>
2022-04-19 16:45:18 -0400

    Complete 13.3.5

--- models/index.js
+++ models/index.js
@@ -1,4 +1,14 @@
 const User = require('./User')
 const Post = require('./Post')
 
+// create association where User has many posts
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+})
+
+// create association where a Post can have only one user
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+})
+
 module.exports = { User, Post }
