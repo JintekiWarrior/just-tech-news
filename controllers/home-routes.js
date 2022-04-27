35c94a3442ff0d32eb595a5afa3e8083b9735033
Moe Yassine <moeyassine1993@gmail.com>
2022-04-27 11:37:30 -0400

    Complete lesson 14.1 - Set up handlebars - Add webpage to view posts

---
+++ controllers/home-routes.js
@@ -0,0 +1,42 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection')
+const { Post, User, Comment } = require('../models')
+
+router.get('/', (req, res) => {
+  Post.findAll({
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      // renders homepage data to the screen, plain = true so that you don't get unecessary sequelize data 
+      // need to map as there is multiple posts being rendered
+      const posts = dbPostData.map(post => post.get({ plain: true }))
+      // pass posts back into object form
+      res.render('homepage', { posts })
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
